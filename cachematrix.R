de9b0889ab104c510fdfeb57fd24f1c211b5acb7
UsmanMu <usmuh@yahoo.com>
2015-01-23 23:49:22 +0200

    Edited cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,18 @@
-Matrix inversion is usually a costly computation 
-If the matrix has been cached before, the inverse of a matrix
-will be time saving ratherthan compute it repeatedly 
+## Matrix inversion is usually a costly computation 
+## If the matrix has been cached before, the inverse of a matrix
+## will be time saving ratherthan compute it repeatedly 
 
-The following two functions used to find the inverse of a matrix
+## The following two functions used to find the inverse of a matrix
 
-makeCacheMatrix  
-cacheSolve
+## makeCacheMatrix  
+## cacheSolve
 
-The first function, makeCacheMatrix creates a special matrix object 
-that can cache its inverse, is a list of functions that can
-  set the value of value of matrix
-  get the value of matrix
-  set the inverse of the matrix
-  get the inverse of the matrix
+## The first function, makeCacheMatrix creates a special matrix object 
+## that can cache its inverse, is a list of functions that can
+ ## set the value of value of matrix
+ ## get the value of matrix
+ ## set the inverse of the matrix
+ ## get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -28,10 +28,10 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getInverse)  
 }
 
-The next function, cacheSolve return the inverse of the above matrix 'x'
-created with the makeCacheMatrix function. If the cached inverse has 
-been computed and available, cacheSolve retrieves it and skips the computation. 
-If not, the inverse of matrix is computed, saved to caches, and returned.
+## The next function, cacheSolve return the inverse of the above matrix 'x'
+## created with the makeCacheMatrix function. If the cached inverse has 
+## been computed and available, cacheSolve retrieves it and skips the computation. 
+## If not, the inverse of matrix is computed, saved to caches, and returned.
 
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
